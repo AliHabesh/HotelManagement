3dfbc4c5f15ef2d621bec2a43445b30c2833c3e6
AkwukwaegbuNneamakaClara <nneamaka_clara.akwukwaegbu0001@stud.hkr.se>
2019-12-05 19:19:22 +0100

    New method in HotelLogic

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -3,6 +3,18 @@ package Task1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+      /*  int a = 10;
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i);
+            }
+        }*/
+
+        int b = 10;
+        while
+        (b > 0) {
+            b--;
+            System.out.print(8 - --b);
+        }
     }
 }
